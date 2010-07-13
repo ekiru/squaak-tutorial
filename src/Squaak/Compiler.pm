57f6c2eb7581dc16a554eb599146f418e24ecfda
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-12 20:33:34 -0500

    Add a command-line banner.

--- src/Squaak/Compiler.pm
+++ src/Squaak/Compiler.pm
@@ -4,4 +4,5 @@ INIT {
     Squaak::Compiler.language('Squaak');
     Squaak::Compiler.parsegrammar(Squaak::Grammar);
     Squaak::Compiler.parseactions(Squaak::Actions);
+    Squaak::Compiler.commandline_banner("Squaak for Parrot VM.\n");
 }

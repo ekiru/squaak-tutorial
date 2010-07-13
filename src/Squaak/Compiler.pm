ae9794719a1c7d7f57e082b6950ecd8e94effdcd
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-12 19:15:25 -0500

    Start working on updating the Squaak tutorial.

---
+++ src/Squaak/Compiler.pm
@@ -0,0 +1,7 @@
+class Squaak::Compiler is HLL::Compiler;
+
+INIT {
+    Squaak::Compiler.language('Squaak');
+    Squaak::Compiler.parsegrammar(Squaak::Grammar);
+    Squaak::Compiler.parseactions(Squaak::Actions);
+}
